b33d1d572f830995fda19374a27177b39d2a03f4
YonyChaparro <ychaparro@unal.edu.co>
2024-07-19 23:53:44 -0500

    finalizado los checkinputs

--- main.js
+++ main.js
@@ -11,33 +11,63 @@ titleToDo.textContent  = 'ToDo Chido';
 const subtitelNombreTarea = document.createElement('h3');
 subtitelNombreTarea.textContent  = 'Ingrese nueva tarea';
 
+//Boton para agregar tarea
+const botonAgregar = document.createElement('button');
+botonAgregar.setAttribute('id', 'botonAgregar');
+botonAgregar.setAttribute('onclick', 'capturarTexto()');
+botonAgregar.type = 'button';
+botonAgregar.textContent = 'Agregar Tarea';
+
+//Subtitulo de tareas pendientes
+const subtitleTareasPendientes= document.createElement('h3')
+subtitleTareasPendientes.textContent = 'Tareas pendientes:'
+
 //Input del título de la tarea
 const entradaTexto = document.createElement('input');
 entradaTexto.type = 'text';
 entradaTexto.id = 'mi-entrada-de-texto';
 entradaTexto.placeholder = 'Escribe aquí...';
 
-//Boton para agregar tarea
-const botonAgregar = document.createElement('button');
-botonAgregar.setAttribute('id', 'botonAgregar');
-botonAgregar.type = 'button';
-botonAgregar.textContent = 'Agregar Tarea';
-
 //Tareas listadas
-const listaTareas = document.createElement('section');
-listaTareas.setAttribute('id', 'listaTareas');
 const ul = document.createElement('ul');
-const items = ['Tarea1', 'Tarea2', 'Tarea3'];
+ul.id = 'ul';
 
-for (const item of items) {
-    const li = document.createElement('li');
-    li.id = 'li';
-    li.textContent = item;
-    ul.appendChild(li);
+const elementos = [];
+
+
+function capturarTexto() {
+    if (entradaTexto.value !=='') {
+        const listaDeElementos = document.getElementById('ul');
+        const nuevoElemento = document.createElement('li');
+        const campoIngresado = entradaTexto.value;
+        const miCasilla = document.createElement('input');
+        miCasilla.setAttribute('id','miCasilla');
+        miCasilla.setAttribute('type','checkbox')
+        elementos.push(campoIngresado);
+        entradaTexto.value=''
+
+        for (elemento in elementos){
+            nuevoElemento.textContent = campoIngresado;
+            listaDeElementos.appendChild(miCasilla);
+            listaDeElementos.appendChild(nuevoElemento);
+        }
+
+        miCasilla.addEventListener('change', function() {
+            if (miCasilla.checked) {
+                nuevoElemento.setAttribute('id', 'textoTachado')
+            }
+      });
+      miCasilla.addEventListener('change', function() {
+        if (!miCasilla.checked) {
+            nuevoElemento.setAttribute('id', 'noTextoTachado')
+        }
+  });
+
+
+    }else{
+        alert('Ingrese tarea loko')
+    }
 }
-
-listaTareas.appendChild(ul);
-
 
 
 //Añadiendo hijos al div contenedor de todo
@@ -45,10 +75,10 @@ contenedor.appendChild(titleToDo)
 contenedor.appendChild(subtitelNombreTarea)
 contenedor.appendChild(entradaTexto);
 contenedor.appendChild(botonAgregar);
-contenedor.appendChild(listaTareas);
-listaTareas.appendChild(ul)
-ul.appendChild(li)
+contenedor.appendChild(subtitleTareasPendientes);
+contenedor.appendChild(ul);
 
 
 
 //****************************Marcar Tarea como Completada:****************************
+
